9ff48e70198586d57c5f204cdbf8890dd0a453dd
Samir <mr.sam.kiev@email.com>
2017-02-10 21:28:00 +0200

    new ver for dev

--- poker.py
+++ poker.py
@@ -1,132 +1,129 @@
-#!/usr/bin/python3.5
+#!/usr/bin/python3.6
 # coding utf-8
 
-"""
-ENG: Poker - Texas Hold'em
-RUS: Покер - Техасский Холдем
-"""
 import random
+import itertools
+from collections import deque
+
+from enum import Enum, auto
+
+# comment this to get real random
+random.seed(11)
+
+class Suits(Enum):
+    SPADE = auto()
+    HEART = auto()
+    CLOVER = auto()
+    TILES = auto()
+
+ranks = list(range(2, 15))
+
+class Deck():
+    def __init__(self):
+        # be careful! deck is not shuffled after init
+        self.deck = list(itertools.product(ranks, Suits))
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def pop(self, n):
+        try:
+            return [self.deck.pop() for _ in range(n)]
+        except IndexError:
+            raise IndexError('deck is empty!!!')
+
+def street_catcher(user_set):
+    """mechanism for catching streets. Will (hopefully) return all streets included in user_set"""
+
+    # user_set is of format [(rank, suite), ....]
+    # we wanna use sets later, so packing it into dict
+    # user_ranks = {
+    #      rank: list(of suits )
+    # }
+    # ex:
+    # user_set = [(2, spade), (3, spade), (2, hearts)]
+    # will turn into
+    # user_ranks = {
+    #     2: [spade, hearts],
+    #     3: [spade]
+    # }
+    # we will need to turn it back to tuples in the end
+
+    user_ranks = dict.fromkeys([r for r, s in user_set], ())
+    for r, s in user_set:
+        user_ranks[r] += (s,)
+
+    street_len = 5
+    ranks_deq = deque(ranks)
+
+    streets = []
+    for _ in range(len(ranks)):
+        street = set(list(ranks_deq)[:street_len])
+        if street <= user_ranks.keys():
+            streets.append(street)
+        ranks_deq.rotate()
+
+    # i know its not too readable
+    catched_streets = \
+        [list(itertools.product(*[[(rank, suit) for suit in user_ranks[rank]] for rank in s])) for s in streets]
+    catched_streets = [j for i in catched_streets for j in i]
+
+    return catched_streets
+
+# these are good object for unittests
+
+def pair(user_set):
+    user_ranks = [r for r, s in user_set]
+    return bool([r for r in ranks if user_ranks.count(r) == 2])
+
+def double_pair(user_set):
+    user_ranks = [r for r, s in user_set]
+    return 1 < len([r for r in ranks if user_ranks.count(r) == 2])
+
+def triple(user_set):
+    user_ranks = [r for r, s in user_set]
+    return bool([r for r in ranks if user_ranks.count(r) == 3])
+
+def kare(user_set):
+    user_ranks = [r for r, s in user_set]
+    return bool([r for r in ranks if user_ranks.count(r) == 4])
+
+def full(user_set):
+    user_ranks = [r for r, s in user_set]
+    ranks_count = {r: user_ranks.count(r) for r in ranks}
+    return all([3 in ranks_count, 2 in ranks_count])
+
+def flash(user_set):
+    user_suits = [r for r, s in user_set]
+    return bool([s for s in Suits if user_suits.count(s) == 5])
+
+def street(user_set):
+    return bool(street_catcher(user_set))
 
 
-def deck_new():
-        """ENG: It creates the deck
-        RUS: Создает колоду"""
-        deck = []
-        ranks = [
-                        "2", "3", "4", "5", "6", "7", "8", "9",
-                        "10", "jack", "queen", "king", "ace"]
-        # black symbols
-        suits = [chr(0x2660), chr(0x2665), chr(0x2663), chr(0x2666)]
-        # white symbols
-        # suits = [chr(0x2664), chr(0x2661), chr(0x2667), chr(0x2662)]
+combinations = {
+    #format is
+    #combination check function (return bool),  : combination name (return bool), combination weight
+    pair          : ('pair', 50),
+    double_pair   : ('double_pair', 100),
+    triple        : ('triple', 150),
+    street        : ('street', 200),
+    flash         : ('flash', 250),
+    full          : ('full', 300),
+    kare          : ('kare', 350),
+}
 
-        for suit in suits:
-                for rank in ranks:
-                        card = (rank, suit)
-                        deck.append(card)
-        return(deck)
+def get_user_result(table, user_set):
+    succeed = [check for check in combinations if check(table + user_set)]
+    return sorted(succeed, key=lambda x: combinations[x][1])[-1] # i know this ugly
 
 
-def deck_rand():
-        """ENG: It shuffle the deck
-        RUS: Перемешивает колоду"""
-        deck = deck_new()
-        deck_rand = []
-        while len(deck) > 0:
-                card = random.choice(deck)
-                deck.remove(card)
-                deck_rand.append(card)
-        # print(len(deck_rand))
-        return deck_rand
+if __name__ == '__main__':
+    deck = Deck()
+    deck.shuffle()
+
+    table = deck.pop(5)
+    u1 = deck.pop(2)
+    u2 = deck.pop(2)
 
 
-def result_math(a):
-        if a == "2":
-                return int(2)
-        elif a == "3":
-                return int(3)
-        elif a == "4":
-                return int(4)
-        elif a == "5":
-                return int(5)
-        elif a == "6":
-                return int(6)
-        elif a == "7":
-                return int(7)
-        elif a == "8":
-                return int(8)
-        elif a == "9":
-                return int(9)
-        elif a == "10":
-                return int(10)
-        elif a == "jack":
-                return int(11)
-        elif a == "queen":
-                return int(12)
-        elif a == "king":
-                return int(13)
-        elif a == "ace":
-                return int(14)
-
-        
-def result_pairs(user_card, table_card):
-        """ENG: Finds and count pairs cards.
-        RUS: Находит и считает парные карты."""
-        result = 0
-        if user_card[0][0].count(user_card[1][0]):
-                result += result_math(user_card[1][0])
-        while len(table_card) > 0:
-                card = table_card[0][0]
-                if user_card[0][0].count(card):
-                        result += result_math(card)
-                if user_card[1][0].count(card):
-                        result += result_math(card)
-                table_card.remove(table_card[0])
-        return int(result)
-
-
-def result_bigcard(user_card):
-        """ENG: Finds and count big card.
-        RUS: Находит и считает большие карты."""
-        result = 0
-        if result_math(user_card[0][0]) > result_math(user_card[1][0]):
-                result = result_math(user_card[0][0])
-        else:
-                result = result_math(user_card[1][0])
-        return int(result)
-
-# print(poker_deck())
-deck = deck_rand()
-table = deck[0:5]
-user1 = deck[5:7]
-user2 = deck[7:9]
-
-print("Table: ", table[0:3])
-print("User1: ", user1)
-print("User2: ", user2)
-input("move1 \n")
-print("Table: ", table[0:4])
-print("User1: ", user1)
-print("User2: ", user2)
-input("move2 \n")
-print("Table: ", table[0:5])
-print("User1: ", user1)
-print("User2: ", user2)
-input("move3 \n")
-
-print("Score: The winner, who has a lot of pair cards!!!")
-p1 = result_pairs(user1[:], table[:])
-p2 = result_pairs(user2[:], table[:])
-
-if p1 == p2:
-        print("Score: The winner, who has a big card!!!")
-        p1 = result_bigcard(user1[:])
-        p2 = result_bigcard(user2[:])
-
-print("Score: p1 =", p1, " p2 =", p2)
-if p1 > p2:
-        print("Winner is pleyer1!!!")
-elif p1 < p2:
-        print("Winner is pleyer2!!!")
-else:
-        print("Friendship wins!!!")
