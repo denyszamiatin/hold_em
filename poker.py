da480dfd2d1e059a4755de2a71cb45a3e1558ee4
butihinda <syncnclock@gmail.com>
2017-02-14 00:24:21 +0200

    added CardsSet class some of the checker-functions changed accordingly

--- poker.py
+++ poker.py
@@ -1,14 +1,16 @@
-#!/usr/bin/python3.6
-# coding utf-8
+from collections import UserDict, defaultdict
 
-import random
-import itertools
 from collections import deque
+
+import itertools
+import random
 
 from enum import Enum, auto
 
-# comment this to get real random
-#random.seed(11)
+# TODO: ANY of poker hands is a 5-card combination - so each combination checking function have to return list of 5 cards.
+
+# TODO: probably each combination can have its own type, types need to have defined priority against each other
+# TODO: each type need to have comparison function to compare against combination of the same type
 
 class Suits(Enum):
     SPADE = auto()
@@ -16,120 +18,119 @@ class Suits(Enum):
     CLOVER = auto()
     TILES = auto()
 
-ranks = list(range(2, 15))
+
+RANKS = list(range(2, 15))
 
 class Deck():
     def __init__(self):
-        # be careful! deck is not shuffled after init
-        self.deck = list(itertools.product(ranks, Suits))
-
-    def shuffle(self):
+        self.deck = list(itertools.product(RANKS, Suits))
         random.shuffle(self.deck)
 
-    def pop(self, n): # maybe we should call it smt like deal ?
+    def pop(self, n):
         try:
-            return [self.deck.pop() for _ in range(n)]
+            result = CardsSet()
+            for _ in range(n):
+                r, s = self.deck.pop()
+                result += CardsSet({r: (s,)})
+            return result
+
         except IndexError:
             raise IndexError('deck is empty!!!')
 
-def street_catcher(user_set):
+
+class CardsSet(UserDict):
+    def __init__(self, cards_set_like=None):
+        self.data = dict.fromkeys(RANKS, ())
+
+        if cards_set_like:
+            for r, s in cards_set_like.items():
+                self.data[r] += s
+
+    def __add__(self, other):
+        if not isinstance(other, CardsSet):
+            raise ValueError('Only CardsSet can be added to cards set')
+
+        return CardsSet({rank: suites + other[rank] for rank, suites in self.data.items()})
+
+    @property
+    def ranks(self):
+        return [rr for r, s in self.data.items() for rr in [r] * len(s)]
+
+# straights
+
+def straights_catcher(user_set: CardsSet):
     """mechanism for catching streets. Will (hopefully) return all streets included in user_set"""
-
-    # user_set is of format [(rank, suite), ....]
-    # we wanna use sets later, so packing it into dict
-    # user_ranks = {
-    #      rank: list(of suits )
-    # }
-    # ex:
-    # user_set = [(2, spade), (3, spade), (2, hearts)]
-    # will turn into
-    # user_ranks = {
-    #     2: [spade, hearts],
-    #     3: [spade]
-    # }
-    # we will need to turn it back to tuples in the end
-
-    user_ranks = dict.fromkeys([r for r, s in user_set], ())
-    for r, s in user_set:
-        user_ranks[r] += (s,)
+    #TODO: https://www.contrib.andrew.cmu.edu/~gc00/reviews/pokerrules#straight
+    user_ranks = set(user_set.ranks)
 
     street_len = 5
-    ranks_deq = deque(ranks)
+    ranks_deq = deque(RANKS)
 
-    streets = []
-    for _ in range(len(ranks)):
+    catched_streets = []
+    for _ in range(len(RANKS)):
         street = set(list(ranks_deq)[:street_len])
-        if street <= user_ranks.keys():
-            streets.append(street)
+        if street <= user_ranks:
+            catched_streets.append(street)
         ranks_deq.rotate()
-
-    # i know its not too readable
-    catched_streets = \
-        [list(itertools.product(*[[(rank, suit) for suit in user_ranks[rank]] for rank in s])) for s in streets]
-    catched_streets = [j for i in catched_streets for j in i]
 
     return catched_streets
 
-# these are good object for unittests
-def biggest_card(user_set):
-    user_ranks = [r for r, s in user_set]
-    return max(user_ranks)
+def straight_flush(user_set: CardsSet):
+    """ return is a list of straight_flushes
+    each straight flush is a tuple (straight_contents)
+    """
 
-def pair(user_set):
-    user_ranks = [r for r, s in user_set]
-    return bool([r for r in ranks if user_ranks.count(r) == 2])
+    straights = straights_catcher(user_set)
 
-def double_pair(user_set):
-    user_ranks = [r for r, s in user_set]
-    return 1 < len([r for r in ranks if user_ranks.count(r) == 2])
+    str_flushes = []
+    for suite in Suits:
+        for strght in straights:
+            candidate = [r for r in strght if suite in user_set[r]]
+            if 5 == len(candidate):
+                str_flushes.append(candidate)
 
-def triple(user_set):
-    user_ranks = [r for r, s in user_set]
-    return bool([r for r in ranks if user_ranks.count(r) == 3])
+    return str_flushes
 
-def street(user_set):
-    user_ranks = [r for r, s in user_set]
-    return (max(user_ranks)-min(user_ranks) == 4) and len(set(user_ranks)) == 5
-
-def flash(user_set):
-    user_suits = set(s for r, s in user_set)
-    return len(user_suits) == 1
-
-def full(user_set):
-    user_ranks = [r for r, s in user_set]
-    ranks_count = {r: user_ranks.count(r) for r in ranks}
-    return all([3 in ranks_count.values(), 2 in ranks_count.values()])
-
-def kare(user_set):
-    user_ranks = [r for r, s in user_set]
-    return bool([r for r in ranks if user_ranks.count(r) == 4])
-
-def street_flash(user_set):
-    return street(user_set) and flash(user_set)
+def royal_flush(user_set: CardsSet):
+    return bool([f for f in straight_flush(user_set) if f == RANKS[-5:]])
 
 
-combinations = {
-    #format is
-    #combination check function (return bool),  : combination name (return bool), combination weight
-    pair          : ('pair', 50),
-    double_pair   : ('double_pair', 100),
-    triple        : ('triple', 150),
-    street        : ('street', 200),
-    flash         : ('flash', 250),
-    full          : ('full', 300),
-    kare          : ('kare', 350),
-}
+def pair(user_set: CardsSet):
+    user_ranks = user_set.ranks
+    return [r for r in RANKS if user_ranks.count(r) == 2]
 
-def get_user_result(table, user_set):
-    succeed = [check for check in combinations if check(table + user_set)]
-    return sorted(succeed, key=lambda x: combinations[x][1])[-1] # i know this ugly
+def two_pairs(user_set: CardsSet):
+    #TODO: need to choose two highest pairs here as hand can contain more than 2
+    return 2 <= len(pair(user_set))
 
 
 if __name__ == '__main__':
-    deck = Deck()
-    deck.shuffle()
+    a = Deck()
+    u1 = CardsSet(a.pop(2))
+    u2 = CardsSet(a.pop(2))
+    table = CardsSet(a.pop(5))
 
-    table = deck.pop(5)
-    u1 = deck.pop(2)
-    u2 = deck.pop(2)
+    b = CardsSet({4: (5, 6, 7, 8), 5: (9, 10)})
 
+
+    # check straights
+    print(royal_flush(CardsSet({
+        9 :(Suits.SPADE,),
+        10:(Suits.SPADE,),
+        11:(Suits.SPADE,),
+        12:(Suits.SPADE,),
+        13:(Suits.SPADE,),
+        14:(Suits.SPADE,),
+    })))
+
+    # check pairs
+
+    print(two_pairs(
+        CardsSet({
+                9 :(Suits.SPADE, Suits.HEART),
+                10:(Suits.SPADE,),
+                11:(Suits.SPADE,),
+                12:(Suits.SPADE, Suits.CLOVER),
+                13:(Suits.SPADE,),
+                14:(Suits.SPADE,),
+        })))
